501db9bd78004e8729f794ad60d32759ffeb00a0
adrianvidal10 <adrian.vidalmo@elorrieta-errekamari.com>
2019-12-02 12:22:52 +0100

    entrada estandar windows 02-12-2019

--- AccesoAdatos/src/main/java/com/AccesoAdatos/dam/AccesoAdatos/App.java
+++ AccesoAdatos/src/main/java/com/AccesoAdatos/dam/AccesoAdatos/App.java
@@ -1,9 +1,30 @@
 package com.AccesoAdatos.dam.AccesoAdatos;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.util.Scanner;
+
+public class App {
+	public static void main(String[] args) {
+		String titulo, autor, genero, fechaPubli, editorial;
+		int edicion, ncopias;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduzca los siguientes datos sobre el libro:");
+		System.out.print("Titulo: ");
+		titulo = sc.nextLine();
+		System.out.print("Autor: ");
+		autor = sc.nextLine();
+		System.out.print("Genero: ");
+		genero = sc.nextLine();
+		System.out.print("Fecha de publicacion: "); 
+		fechaPubli = sc.nextLine();
+		System.out.print("Editorial: ");
+		editorial = sc.nextLine();
+		System.out.print("Edicion: ");
+		edicion = sc.nextInt();
+		sc.nextLine();
+		System.out.print("Numero de copias: ");
+		ncopias = sc.nextInt();
+		sc.nextLine();
+		sc.close();
+		
+	}
 }
